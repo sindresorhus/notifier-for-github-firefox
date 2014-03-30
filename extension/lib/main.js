cf6824857483e5fb86a9f3026200887c1e2480b4
Sindre Sorhus <sindresorhus@gmail.com>
2014-03-30 01:04:33 +0100

    init

---
+++ extension/lib/main.js
@@ -0,0 +1,75 @@
+'use strict';
+const tabs = require('sdk/tabs');
+const data = require('sdk/self').data;
+const Request = require('sdk/request').Request;
+const pageWorker = require('sdk/page-worker');
+const timers = require('sdk/timers');
+const toolbarButton = require('toolbarbutton/toolbarbutton').ToolbarButton;
+
+const notifUrl = 'https://github.com/notifications';
+const updateInterval = 1000 * 60;
+
+function update() {
+	Request({
+		url: notifUrl,
+		onComplete: function (response) {
+			worker.port.emit('render', response.text);
+		}
+		// Need to add a check if the computer is connected to the internet.
+		// Waiting on a `onError` method.
+	}).get();
+};
+
+let worker = pageWorker.Page({
+	contentScriptFile: data.url('icon.js')
+});
+
+let tbb = toolbarButton({
+	id: 'github-notifier',
+	label: 'GitHub Notifier',
+	tooltiptext: this.label,
+	image: data.url('icon-16.png'),
+	onCommand: function () {
+		const tab = tabs.activeTab;
+
+		if (tab.url === 'about:blank' || tab.url === 'about:newtab' || tab.url === notifUrl) {
+			tab.url = notifUrl;
+		} else {
+			tabs.open(notifUrl);
+		}
+
+		timers.setTimeout(update, 1000 * 20);
+		update();
+	}
+});
+
+tbb.moveTo({
+	toolbarID: 'nav-bar',
+	forceMove: false // only move from palette
+});
+
+worker.port.on('fetched-count', function (count) {
+	count = count > 999 ? '∞' : count;
+
+	if (count) {
+		tbb.tooltiptext = 'GitHub Notifier';
+
+		if (count !== '0') {
+			tbb.badge = {
+				text: count,
+				color: 'rgb(65, 131, 196)'
+			}
+		} else {
+			tbb.badge = null;
+		}
+	} else {
+		tbb.tooltiptext = 'You have to be logged into GitHub';
+		tbb.badge = {
+			text: '?',
+			color: 'rgb(166, 41, 41)'
+		}
+	}
+});
+
+timers.setInterval(update, updateInterval);
+update();
---
+++ readme.md
@@ -0,0 +1,20 @@
+# GitHub Notifier for Firefox
+
+> Extension that displays your GitHub notifications unread count
+
+![](screenshot.png)
+
+
+## Install
+
+Install from the [Firefox Add-ons site](https://addons.mozilla.org/en-US/firefox/addon/github-notifier/).
+
+
+## Related
+
+Also available on [Chrome](https://github.com/sindresorhus/github-notifier-chrome), [Opera](https://github.com/sindresorhus/github-notifier-opera), [Safari](https://github.com/sindresorhus/github-notifier-safari).
+
+
+## License
+
+[MIT](http://opensource.org/licenses/MIT) © [Sindre Sorhus](http://sindresorhus.com)
