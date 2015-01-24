c04f2e1d4da505a292c2831742ce961417673b07
Aleksandr Seleznev <seleznev.ru@gmail.com>
2015-01-24 19:09:27 +0600

    Add 32px and 64px icons

--- extension/lib/main.js
+++ extension/lib/main.js
@@ -29,6 +29,8 @@ let tbb = ActionButton({
 	label: 'GitHub Notifier',
 	icon: {
 		'16': './icon-16.png',
+		'32': './icon-32.png',
+		'64': './icon-64.png',
 	},
 	onClick: function (state) {
 		const tab = tabs.activeTab;
